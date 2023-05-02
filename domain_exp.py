9aa123c198d069840d0c1ac2894e16f2a922a3fe
Kate <38916356+kate7000@users.noreply.github.com>
2023-05-02 18:38:27 +0300

    Update domain_exp.py
    Add condition

--- domain_exp.py
+++ domain_exp.py
@@ -10,8 +10,11 @@ domains_list = [x.replace('\n', '') for x in file_list]
 domain_dict = {}
 for d in domains_list:
     w = whois.whois(d)
-    if w.expiration_date is not None:
+    if w.expiration_date is not None and type(w.expiration_date) is not list:
         diff_days = w.expiration_date - datetime.now()
+        domain_dict[d] = diff_days.days
+    elif type(w.expiration_date) == list:
+        diff_days = w.expiration_date[0] - datetime.now()
         domain_dict[d] = diff_days.days
     #print('{', '\n','"name":','"%s",' % d,'\n','"days":','"%s",' % diff_days.days,'\n','},')
     
